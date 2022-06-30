b6eca3c7fc68837d03bda46e71e83c4403cbe47e
Sergey_Stepanov <cmi52@mail.ru>
2022-07-01 01:00:29 +0300

    HW2.2 done

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
         Car car2 = new Car("car2", 4);
 
 
-        Truck truck = new Truck("track1", 6);
+        Truck truck = new Truck("track1", 10);
         Truck truck2 = new Truck("track2", 8);
 
 
@@ -12,11 +12,11 @@ public class Main {
         Bicycle bicycle2 = new Bicycle("bicycle2", 2);
 
         ServiceStation station = new ServiceStation();
-        station.check(car, null, null);
-        station.check(car2, null, null);
-        station.check(null, bicycle, null);
-        station.check(null, bicycle2, null);
-        station.check(null, null, truck);
-        station.check(null, null, truck2);
+        station.check(car);
+        station.check(car2);
+        station.check( bicycle);
+        station.check( bicycle2);
+        station.check(truck);
+        station.check(truck2);
     }
 }
